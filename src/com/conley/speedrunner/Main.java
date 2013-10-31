e516b86b6ec3ab57866759d0dabaa5e2d691edb9
Chris Conley <chrisc93@vt.edu>
2013-10-31 11:17:55 -0400

    Refactoring Signed-off-by: Chris Conley <chrisc93@vt.edu>

--- src/com/conley/speedrunner/Main.java
+++ src/com/conley/speedrunner/Main.java
@@ -1,7 +1,8 @@
-package com.conley.speedrider;
+package com.conley.speedrunner;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.conley.speedrunner.SpeedRider;
 
 public class Main {
 	public static void main(String[] args) {
